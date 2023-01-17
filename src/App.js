7ca24d231d5fec784d2a34b54f494a961269cd70
Oleksandr Solianyk <salym3k@gmail.com>
2023-01-17 20:11:54 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ListButton from "./component/ListButton";
+import Form from "./component/Form";
+
+function App() {
+  return (
+    <div className='container'>
+      <ListButton/>
+        <hr/>
+        <Form/>
+    </div>
+  );
+}
+
+export default App;
